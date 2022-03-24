e322011da376ddfa5e327d1f855460116c780c70
mac11_lc3 <cecyparamo94@gmail.com>
2022-03-24 11:18:05 -0600

    Clase niños agregando metodos y atributos

--- src/Chamaca.java
+++ src/Chamaca.java
@@ -1,2 +1,27 @@
-public class Chamaca {
+public class Chamaca extends Personaje{
+
+    //Color puerta
+    private String colorPuerta;
+
+    public String getColorPuerta() {
+        return colorPuerta;
+    }
+
+    public void setColorPuerta(String colorPuerta) {
+        this.colorPuerta = colorPuerta;
+    }
+
+    public Chamaca(String nombre, double estatura, double opeso, String colorPuerta) {
+        super(nombre, estatura, opeso);
+        this.colorPuerta = colorPuerta;
+    }
+
+    //Reir
+    //Llorar
+    public void reir(){
+        System.out.println("Jajajajaja");
+    }
+    public void llorar(){
+        System.out.println("WEEEEEE, WEEEEE ");
+    }
 }
